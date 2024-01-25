807b2d1b81739ed7cf0060a35513753d409ce352
joshhigg <136753578+joshhigg@users.noreply.github.com>
2024-01-25 15:15:57 -0700

    added resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,15 +1,24 @@
 export default function Resume() {
     return (
       <div>
-        <h1>Resume</h1>
-        <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-        </p>
+        <h3 className="resume-title">Download my resume <a href="src/assets/josh.higginson.resume.pdf" download className="resume">here</a></h3>
+        <h4>Technical Skills</h4>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Javascript</li>
+          <li>Node</li>
+          <li>React</li>
+          <li>JQuery</li>
+          <li>Express</li>
+          <li>MongoDB</li>
+          <li>mySQL</li>
+          <li>noSQL</li>
+          <li>Handlebars</li>
+          <li>Bootstrap</li>
+          <li>Tailwind</li>
+          <li>APIs</li>
+        </ul>
       </div>
     );
   }
