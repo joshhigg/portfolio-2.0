804addb0f457f979d520634a786280f3b06e9bf8
joshhigg <136753578+joshhigg@users.noreply.github.com>
2024-01-26 11:05:37 -0700

    removed form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,105 +1,105 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
-export default function Contact(props) {
-  const [nameInput, setName] = useState('');
-  const [emailInput, setEmail] = useState('');
-  const [messageInput, setMessage] = useState('');
-  const [nameError, setNameError] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [messageError, setMessageError] = useState('');
+export default function Contact() {
+  // const [nameInput, setName] = useState('');
+  // const [emailInput, setEmail] = useState('');
+  // const [messageInput, setMessage] = useState('');
+  // const [nameError, setNameError] = useState('');
+  // const [emailError, setEmailError] = useState('');
+  // const [messageError, setMessageError] = useState('');
 
-  const validateEmail = (email) => {
-    // Email validation regular expression
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  }
+  // const validateEmail = (email) => {
+  //   // Email validation regular expression
+  //   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  //   return emailRegex.test(email);
+  // }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
 
-    // Validate the fields
-    if (!nameInput) {
-      setNameError('Name is required');
-      return;
-    } else {
-      setNameError('');
-    }
+  //   // Validate the fields
+  //   if (!nameInput) {
+  //     setNameError('Name is required');
+  //     return;
+  //   } else {
+  //     setNameError('');
+  //   }
 
-    if (!emailInput || !validateEmail(emailInput)) {
-      setEmailError('Valid email is required');
-      return;
-    } else {
-      setEmailError('');
-    }
+  //   if (!emailInput || !validateEmail(emailInput)) {
+  //     setEmailError('Valid email is required');
+  //     return;
+  //   } else {
+  //     setEmailError('');
+  //   }
 
-    if (!messageInput) {
-      setMessageError('Message is required');
-      return;
-    } else {
-      setMessageError('');
-    }
+  //   if (!messageInput) {
+  //     setMessageError('Message is required');
+  //     return;
+  //   } else {
+  //     setMessageError('');
+  //   }
 
-    // Proceed with form submission
-    props.onSubmit({
-      id: Math.random(Math.floor() * 1000),
-      name: nameInput,
-      email: emailInput,
-      message: messageInput
-    });
+  //   // Proceed with form submission
+  //   props.onSubmit({
+  //     id: Math.random(Math.floor() * 1000),
+  //     name: nameInput,
+  //     email: emailInput,
+  //     message: messageInput
+  //   });
 
-    console.log(props);
+  //   console.log(props);
 
-    // Clear the form
-    setName('');
-    setEmail('');
-    setMessage('');
-  }
+  //   // Clear the form
+  //   setName('');
+  //   setEmail('');
+  //   setMessage('');
+  // }
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
 
-    // Clear the error message when the user starts typing
-    if (name === 'name') {
-      setName(value);
-      setNameError('');
-    } else if (name === 'email') {
-      setEmail(value);
-      setEmailError('');
-    } else if (name === 'message') {
-      setMessage(value);
-      setMessageError('');
-    }
-  }
+  //   // Clear the error message when the user starts typing
+  //   if (name === 'name') {
+  //     setName(value);
+  //     setNameError('');
+  //   } else if (name === 'email') {
+  //     setEmail(value);
+  //     setEmailError('');
+  //   } else if (name === 'message') {
+  //     setMessage(value);
+  //     setMessageError('');
+  //   }
+  // }
 
-  const handleBlur = (e) => {
-    const { name, value } = e.target;
+  // const handleBlur = (e) => {
+  //   const { name, value } = e.target;
 
-    // Validate the fields on blur
-    if (name === 'name' && !value) {
-      setNameError('Name is required');
-    }
+  //   // Validate the fields on blur
+  //   if (name === 'name' && !value) {
+  //     setNameError('Name is required');
+  //   }
 
-    if (name === 'email') {
-      if (!value) {
-        setEmailError('Email is required');
-      } else if (!validateEmail(value)) {
-        setEmailError('Valid email is required');
-      } else {
-        setEmailError('');
-      }
-    }
+  //   if (name === 'email') {
+  //     if (!value) {
+  //       setEmailError('Email is required');
+  //     } else if (!validateEmail(value)) {
+  //       setEmailError('Valid email is required');
+  //     } else {
+  //       setEmailError('');
+  //     }
+  //   }
 
-    if (name === 'message' && !value) {
-      setMessageError('Message is required');
-    }
-  }
+  //   if (name === 'message' && !value) {
+  //     setMessageError('Message is required');
+  //   }
+  // }
 
   return (
     <div>
       <h1>Contact Me</h1>
       <h4>Email: josh.higginson201@gmail.com</h4>
       <h4>Phone: 385-479-5311</h4>
-      <form className='contactForm' onSubmit={handleSubmit}>
+      {/* <form className='contactForm' onSubmit={handleSubmit}>
         <label htmlFor='name'>Name</label>
         <input type="text" id="name" placeholder="your name" name='name' onChange={handleChange} onBlur={handleBlur} value={nameInput}></input>
         {nameError && <div className="error">{nameError}</div>}
@@ -110,7 +110,7 @@ export default function Contact(props) {
         <textarea id="message" name="message" placeholder="your message" onChange={handleChange} onBlur={handleBlur} value={messageInput}></textarea>
         {messageError && <div className="error">{messageError}</div>}
         <input type="submit" value="Submit"></input>
-      </form>
+      </form> */}
     </div>
   );
 }
